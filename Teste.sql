07c93e98809d8e1787e8b2482a5f76aadf8755ae
Clara Valois <clara.cardoso1512@gmail.com>
2022-10-17 02:55:28 -0300

    inventei um teste sql

---
+++ Teste.sql
@@ -0,0 +1,22 @@
+Em produção.
+
+create schema novo;
+use novo;
+
+create table categorias(
+id_categoria integer auto_increment key
+);
+
+create table usuarios(
+id_user integer auto_increment key,
+nome_user varchar (30),
+senha varchar(10),
+cpf varchar (15), 
+email_user varchar (120),
+telefone varchar (11),
+nivel varchar (15));
+
+
+
+ 
+ select * from usuarios;
