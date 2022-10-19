9ba9467589d64a52071935a7f15c9dc4a3a1b525
Clara Valois <clara.cardoso1512@gmail.com>
2022-10-19 11:47:13 -0300

    Novo banco adicioado

--- Teste.sql
+++ Teste.sql
@@ -1,15 +1,10 @@
-create schema novo;
-use novo;
+create schema almox;
+use almox;
 
  create table area_depar(
 id_area integer auto_increment primary key,
 depar varchar (40)
  );
- 
-create table categorias(
-id_categoria integer auto_increment key
-);
-
 create table usuarios(
 id_user integer auto_increment key,
 nome_user varchar (30),
@@ -30,5 +25,86 @@ id_area integer not null,
 nivel varchar (15),
 foreign key (id_area)  references area_depar (id_area)); 
 
+create table categorias (
+id_categoria integer auto_increment key,
+nome_categoria varchar (30)
+);
+    
+
+CREATE TABLE modelo ( 
+ id_modelo integer auto_increment primary key,  
+ descricao_mod varchar(30)); 
+
+CREATE TABLE marca( 
+ descricao_marca  integer,  
+ id_marca integer auto_increment primary key); 
  
- select * from usuarios;
+ CREATE TABLE valorProduto ( 
+ id_valor integer auto_increment primary key,
+ lote integer,  
+ valorLote float); 
+
+CREATE TABLE tipoProduto 
+(  id_tipo integer auto_increment primary key,  
+ descricao_tipo varchar (30)); 
+ 
+create table produto( id_prod INT auto_increment KEY,  
+ quantDisp integer,  
+ nome_prod varchar(30),  
+ datacaptura date,
+ hora_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ id_marca integer not null,  
+ id_modelo integer not null,  
+ id_valor integer not null,
+ id_tipo integer not null,
+ id_categoria integer not null,
+ id_user integer not null,
+ 
+ foreign key  (id_user ) references usuarios (id_user),
+ foreign key  (id_categoria ) references categorias (id_categoria),
+foreign key  (id_marca ) references marca(id_marca ),
+ foreign key  (id_modelo) references modelo (id_modelo),
+ foreign key  (id_valor) references valorProduto(id_valor),
+foreign key  ( id_tipo) references tipoProduto( id_tipo)
+
+); 
+
+CREATE TABLE requisicao( 
+ stats varchar(10),  
+ id_req integer auto_increment primary key, 
+ dataReq date,  
+ 
+ id_user integer not null,
+ id_prod integer not null, 
+ valorT float,
+ 
+foreign key (id_user) references usuarios (id_user),
+foreign key (id_prod) references produto (id_prod)
+); 
+
+CREATE TABLE itemreq ( 
+ quantsol integer not null,  
+ id_req integer not null,  
+ id_prod integer not null, 
+ foreign key (id_req) references requisicao (id_req),
+ foreign key (id_prod) references produto( id_prod)
+); 
+
+CREATE TABLE notafiscal
+( 
+ id_nf integer auto_increment primary key,
+ num_serie integer,  
+ fornecedor varchar(25),  
+ CNPJ integer  
+); 
+
+CREATE TABLE entrada ( 
+ qtd integer,  
+ id_entrada integer auto_increment primary key,
+ id_prod integer not null,  
+ id_nf integer not null,
+ foreign key (id_nf) references notafiscal (id_nf),
+ foreign key (id_prod) references produto (id_prod)
+ ); 
+ 
+
