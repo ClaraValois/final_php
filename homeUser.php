0d12b58d698202de391b4ab9c2f9546593485295
maria.amorim <maria.amorim@dantaseletro.com.br>
2022-10-20 12:17:21 -0300

    refactor: alterações no html

--- homeUser.php
+++ homeUser.php
@@ -1,47 +1,45 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="assets/homeUser.css" />
-  </head>
-  <body>
-    <main class="main">
-      <h1>SEJA BEM-VINDO!</h1>
-     
-      <div class="containerInput">
-        <span id="msg"></span>
-        <form action="" id="form-pesquisar">
-          <div class="searchbox">
-            <input name="texto_pesquisar" id="btn-pesquisar"
-              type="text"
-              class="searchinput"
-              placeholder="Buscar produto desejado..."
-              />
-          </form>
-          <span id="listar-usuarios"></span>
 
-          <button class="searchbutton">
-            <img class="iconSearch"
-              src="/Resources/Assets/Imagens/iconSearch.png" alt="">
-          </button>
-        </div>
-      </div>
-      <!-- <a href="/requisicoes"> -->
-      <button type="submit" class="btn">Requisições</button>
-      <!-- </a> -->
-    </main>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="assets/homeUser.css" />
+  <link rel="stylesheet" href="/assets/template.css">
 
-    <script>
+</head>
 
-   // Receber o seletor form-pesquisar
-const formPesquisar = document.getElementById("form-pesquisar");
+<body>
+  <main class="main">
+    <h1>SEJA BEM-VINDO!</h1>
 
-// Verificar se existe o form-pesquisar
-if (formPesquisar) {
-    // Aguardar o submit, quando o usuário clicar no botão executa a função
-    formPesquisar.addEventListener("submit", async (e) => {
+    <div class="containerInput">
+      <span id="msg"></span>
+      <form action="" id="form-pesquisar">
+        <div class="searchbox">
+          <input name="texto_pesquisar" id="btn-pesquisar" type="text" class="searchinput" placeholder="Buscar produto desejado..." />
+      </form>
+      <span id="listar-usuarios"></span>
+
+      <button class="searchbutton">
+        <img class="iconSearch" src="/assets/images/iconSearch.png" alt="icone search">
+      </button>
+    </div>
+    </div>
+    <!-- <a href="/requisicoes"> -->
+    <button type="submit" class="btn">Requisições</button>
+    <!-- </a> -->
+  </main>
+
+  <script>
+    // Receber o seletor form-pesquisar
+    const formPesquisar = document.getElementById("form-pesquisar");
+
+    // Verificar se existe o form-pesquisar
+    if (formPesquisar) {
+      // Aguardar o submit, quando o usuário clicar no botão executa a função
+      formPesquisar.addEventListener("submit", async (e) => {
 
         // Bloquear para não recarregar a página
         e.preventDefault();
@@ -58,9 +56,9 @@ if (formPesquisar) {
         }*/
 
         // Fazer a requisição para o arquivo pesquisar.php
-        const dados = await fetch("pesquisar.php",{
-            method: "POST",
-            body: dadosForm
+        const dados = await fetch("pesquisar.php", {
+          method: "POST",
+          body: dadosForm
         });
 
         // Ler os dados retornado do arquivo pesquisar.php
@@ -68,21 +66,21 @@ if (formPesquisar) {
         //console.log(resposta);
 
         // Acessa o IF quando não retornar nenhum usuário do banco de dados
-        if(!resposta['status']){
-            // Enviar a mensagem de erro do JavaScript para o HTML
-            document.getElementById("msg").innerHTML = resposta['msg'];
-        }else{
-            // Substituir a mensagem de erro
-            document.getElementById("msg").innerHTML = "";
-            // Enviar a lista de usuário do JavaScript para o HTML
-            document.getElementById("listar-usuarios").innerHTML = resposta['dados'];
+        if (!resposta['status']) {
+          // Enviar a mensagem de erro do JavaScript para o HTML
+          document.getElementById("msg").innerHTML = resposta['msg'];
+        } else {
+          // Substituir a mensagem de erro
+          document.getElementById("msg").innerHTML = "";
+          // Enviar a lista de usuário do JavaScript para o HTML
+          document.getElementById("listar-usuarios").innerHTML = resposta['dados'];
         }
 
         // Substituir o texto do botão para pesquisar
         document.getElementById("btn-pesquisar").value = "Pesquisar";
-    });
-}
-      
-    </script>
-  </body>
+      });
+    }
+  </script>
+</body>
+
 </html>
