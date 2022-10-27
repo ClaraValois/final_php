2b9bd7f2b8c2520462e6eee7b4e4a51f8aa6786c
maria.amorim <mariaclaraamorimm221@gmail.com>
2022-10-27 18:29:19 -0300

    amém

--- View/Pages/Usuario/homeUser.php
+++ View/Pages/Usuario/homeUser.php
@@ -23,7 +23,6 @@
 <body>
 
 <?php include dirname(__DIR__, 3) . "/template/header.php"; ?>
-<?php include dirname(__DIR__, 3) . "/template/aside.php"; ?>
 <?php include dirname(__DIR__, 3) . "/template/footer.php"; ?>
 
   <main class="main">
